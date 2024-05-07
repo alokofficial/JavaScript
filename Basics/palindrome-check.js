90eacf982668132d5f9b7ebab0339182c30df9b1
Alok Kumar <aloknh31@gmail.com>
2024-05-07 18:19:01 +0530

    arrange

---
+++ Basics/palindrome-check.js
@@ -0,0 +1,8 @@
+function isPalindrome(str){ 
+	
+	return (str===str.split('').reverse().join('')) 
+
+} 
+
+console.log(isPalindrome('racecar'))
+console.log(isPalindrome('racecars'))
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 ##The filter() method returns an array of objects and find() method returns a single object.
+
+|* https://www.geeksforgeeks.org/javascript-examples/ *|
